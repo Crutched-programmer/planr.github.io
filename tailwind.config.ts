223b36ab7a699f98884440d03bd5d064f3f758d8
Sashant Sankar (Crutched programmer) <thyareoptimistic@gmail.com>
2025-08-14 11:21:42 +0000

    make it move to the right instead of back and forth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,14 +97,13 @@ export default {
         },
         'gradient-flow': {
           '0%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
+          '100%': { backgroundPosition: '200% 50%' },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'gradient-flow': 'gradient-flow 8s ease infinite',
+        'gradient-flow': 'gradient-flow 8s linear infinite',
       },
     },
   },
