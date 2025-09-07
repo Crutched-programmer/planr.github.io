aff269be14860e2753b15aba1f6c1443a869db73
Sashant Sankar <sashant.san@gmail.com>
2025-09-07 08:04:45 +0000

    make the background black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className={`${inter.variable} ${lexendDeca.variable} font-body antialiased min-h-screen flex flex-col text-foreground`} suppressHydrationWarning>
-        <PixelBlast variant="circle" color="#B19EEF" />
+        <PixelBlast variant="circle" color="#B19EEF" transparent={false} />
         <div className="relative z-10 flex flex-col min-h-screen">
             {children}
             <Toaster />
