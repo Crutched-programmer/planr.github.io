2bdcd25e73c302414b95efa9e83003158f659f19
Sashant Sankar <sashant.san@gmail.com>
2025-09-14 14:39:07 +0000

    make the thing wavy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,14 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className={`${inter.variable} ${lexendDeca.variable} font-body antialiased min-h-screen flex flex-col text-foreground bg-transparent`} suppressHydrationWarning>
-        <DynamicPixelBlast variant="circle" color="#86cc83" transparent={false} />
+        <DynamicPixelBlast 
+            variant="circle" 
+            color="#86cc83" 
+            transparent={false} 
+            liquid={true}
+            liquidStrength={0.15}
+            liquidRadius={0.5}
+        />
         <div className="relative z-10 flex flex-col min-h-screen">
             {children}
             <Toaster />
