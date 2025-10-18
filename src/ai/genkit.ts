001f6e2324280076fd8b4f2450cf121a420d95bc
Sashant Sankar <sashant.san@gmail.com>
2025-10-18 03:24:46 +0000

    initial commit

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: "AIzaSyBGR1kLdVqz_n8cOz5oW7fwBp_1i8N5C-A"})],
+  plugins: [googleAI({apiKey: "YOUR_API_KEY"})],
   model: 'googleai/gemini-2.0-flash',
 });
